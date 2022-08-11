97c4e39815ca521c5318bef4bbb34d24a0120621
veeso <christian.visintin1997@gmail.com>
2022-08-11 10:07:10 +0200

    anyhow

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,22 @@ extern crate tracing;
 mod net;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     info!("jab {} - developed by {}", JAB_VERSION, JAB_AUTHORS);
 
-    let mut node = net::Node::init().await.expect("could not init node");
+    let mut node = match net::Node::init().await {
+        Ok(node) => node,
+        Err(err) => {
+            anyhow::bail!("Failed to initialize node: {}", err.to_string());
+        }
+    };
     info!("node successfully initialized");
     // listen
-    node.listen().expect("failed to start listener");
+    if let Err(err) = node.listen() {
+        anyhow::bail!("Failed to start listener: {}", err.to_string());
+    }
+    info!("listener started");
 
     // loop
     loop {
